4b5896469f96dd334b0d676688f51e88afecae5f
Himangshu Mondal <mailtoherock@gmail.com>
2020-09-10 23:18:57 +0530

    updated html, css and added javascript functionality on th submenu

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const menuBtn = document.querySelector(".menu-button button");
-let menuIcon = document.querySelector(".menu-button button img");
+const menuIcon = document.querySelector(".menu-button button img");
 const naviGation = document.querySelector(".navigation");
+let dropdownToggle = document.querySelectorAll(".dropdown-toggle");
+let dropdownMenu = document.querySelectorAll(".dropdown-menu");
 
-function openNavigation() {
-    menuBtn.addEventListener("click", function () {
-        this.classList.toggle("active");
-        if(menuBtn.classList.contains("active")){
-            menuIcon.src = "./icon/x.svg";
-        }
-        else{
-            menuIcon.src = "./icon/menu.svg";
-        }
-        naviGation.classList.toggle("active"); 
+for (let i = 0; i < dropdownToggle.length; i++) {
+    if(window.innerWidth > 992){
+        dropdownToggle[i].addEventListener("mouseenter", function() {
+            dropdownToggle[i].parentElement.classList.add("active");
+        });
+    }
+    if(window.innerWidth < 991){
+        dropdownToggle[i].addEventListener("click", function() {
+            console.log("hello");
+            dropdownToggle[i].parentElement.classList.toggle("active");
+        });
+    }
+}
+
+for (let i = 0; i < dropdownMenu.length; i++) {
+    dropdownMenu[i].addEventListener("mouseleave", function() {
+        dropdownMenu[i].parentElement.classList.remove("active");
     });
 }
 
-openNavigation();
+function openNavigation() {
+    menuBtn.classList.toggle("active");
+    if(menuBtn.classList.contains("active")){
+        menuIcon.src = "./icon/x.svg";
+    }
+    else{
+        menuIcon.src = "./icon/menu.svg";
+    }
+    naviGation.classList.toggle("active"); 
+}
+menuBtn.addEventListener("click", openNavigation);
+
+
